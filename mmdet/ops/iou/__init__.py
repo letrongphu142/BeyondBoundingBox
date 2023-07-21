3aa574e5ff2cec411923f51b6af9d0525327258f
Letrongphu <Letrongphu142@gmail.com>
2023-07-21 09:39:34 +0700

    update source

--- mmdet/ops/iou/__init__.py
+++
@@ -1,4 +0,0 @@
-from .iou_wrapper import convex_giou
-from .iou_wrapper import convex_iou
-
-__all__ = ['convex_giou', 'convex_iou']
--- mmdet/ops/nms/__init__.py
+++
@@ -1,3 +0,0 @@
-from .nms_wrapper import nms, soft_nms, rnms, soft_rnms
-
-__all__ = ['nms', 'soft_nms', 'rnms', 'soft_rnms']
